2caccf4388604569aeb9afc8672aed4a6ff13a10
Sokos6 <will@comicbooked.com>
2018-08-01 17:37:23 -0500

    css and hero list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,6 +13,8 @@ hero: Hero = {
   id: 1,
   name: 'ColdSnap'
 };
+
+heroes = HEROES;
 
   constructor() { }
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Freezer Bag' },
+  { id: 12, name: 'Neverender' },
+  { id: 13, name: 'Bombai' },
+  { id: 14, name: 'Celeste' },
+  { id: 15, name: 'Magno' },
+  { id: 16, name: 'Rubic' },
+  { id: 17, name: 'Dynamo' },
+  { id: 18, name: 'Dr Boom' },
+  { id: 19, name: 'MagmaRaider' },
+  { id: 20, name: 'Sharknado' }
+];
